5cb1a6e7fe642006d50bdb9158272a7d21613c6d
Manas Hejmadi <manas2all@outlook.com>
2024-05-19 08:24:41 +0530

    Added Credentus Heading

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,14 +4,17 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className="flex flex-row justify-end h-20 p-4 mt-8">
-            <ConnectButton />
-            <button className="border-2 mx-4 rounded-lg border-white px-2 py-1">
-                <Link href="/uploadform">Create</Link>
-            </button>
-            <button className="border-2 mx-2 rounded-lg border-white px-3 py-1">
-                <Link href="/me">Me</Link>
-            </button>
+        <div className="flex flex-row justify-between h-20 p-4 mb-4 items-center">
+            <h1 className='text-white text-5xl ml-4'>Credentus</h1>
+            <div className='flex'>
+                <ConnectButton />
+                <button className="border-2 mx-4 rounded-lg border-white px-2 py-1">
+                    <Link href="/uploadform">Create</Link>
+                </button>
+                <button className="border-2 mx-2 rounded-lg border-white px-3 py-1">
+                    <Link href="/me">Me</Link>
+                </button>
+            </div>
         </div>
     )
 }
